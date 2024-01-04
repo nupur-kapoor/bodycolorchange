7803b40a8261370098cf6ccd719e5af35c336e7f
nupur-kapoor <146827240+nupur-kapoor@users.noreply.github.com>
2024-01-04 17:11:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+// // var form = document.querySelector("form")
+// var h = document.getElementById('height').value;
+// console.log(h)
+// var w = document.getElementById("ww").value;
+// var result = document.querySelector("#result")
+// var b= document.getElementById("bt");
+
+// // form.addEventListener("submit", function(e)
+// // {
+// //        e.preventDefault();
+
+  
+
+//    if (h===1||h<0 )
+//    {
+//       result.innerHTML = h;
+//       console.log(h)
+//    }
+//    else if (w === '' || w< 0 )
+//    {
+//       result.innerHTML = w;
+//       console.log(w)
+//    }
+//   else{
+//       // b.addEventListener(this.onclick , function{
+//       //    var bmi =703*(w/(h*h));
+
+//       //    result.innerHTML = bmi;
+//       // });
+//       b.onclick=function(){
+//          var bmi =703*(w/(h*h));
+//                console.log(bmi)
+//          result.innerHTML = bmi;
+//       }
+    
+//   }
+
+
+// // });
